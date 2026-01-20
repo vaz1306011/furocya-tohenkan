7abb6c424da7e8acbf3cd7f28806eb9e072ee71d
Sora <vaz1306011@gmail.com>
2026-01-21 00:21:07 +0900

    fix: expr to_str

--- furohen/convert/convert.py
+++ furohen/convert/convert.py
@@ -36,9 +36,11 @@ def id_to_str(node: ID) -> Optional[str]:
 # x = 10; -> 10をxになる
 @register_format(Assignment)
 def assignment_to_str(assignment: Assignment) -> Optional[str]:
+    from furohen.convert.convert import to_str
+
     op = assignment.op
     name = assignment.lvalue.name
-    value = assignment.rvalue.value
+    value = to_str(assignment.rvalue)
     if op == "=":
         return f"{name}を{value}になる"
     return None
@@ -62,10 +64,12 @@ def constant_to_str(constant: Constant) -> Optional[str]:
 # int x = 10; -> xを10になる
 @register_format(Decl)
 def decl_to_str(decl: Decl) -> Optional[str]:
+    from furohen.convert.convert import to_str
+
     name = decl.name
     if decl.init is None:
         return f"{name}を宣言"
-    value = decl.init.value
+    value = to_str(decl.init)
     return f"{name}を{value}になる"
 
 
@@ -99,9 +103,11 @@ def if_to_str(if_stmt: If) -> Optional[str]:
 
 @register_format(Return)
 def return_to_str(ret: Return) -> Optional[str]:
+    from furohen.convert.convert import to_str
+
     if ret.expr is None:
         return "終了"
-    value = ret.expr.value
+    value = to_str(ret.expr)
     return f"{value}を返す"
 
 
